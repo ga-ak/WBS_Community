4be220c77449551cab42dd932a7b824ff24036cc
Cheolho kim <gghg0000@gmail.com>
2019-06-30 22:04:16 +0900

    19.06.30 kch_mac
    댓글 계층적으로 표현

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,16 +17,24 @@ import com.example.demo.service.DemoService;
 @Controller
 public class DemoController {
 
-	@Autowired
-	DemoService service;
-	
-	@RequestMapping("/demo.do")
-	public ModelAndView demo(HttpServletRequest req, HttpServletResponse resp) {
-		ModelAndView mav = new ModelAndView();
-		
-		List<String> idList = service.selectAllId();
-		mav.addObject("idList", idList);
-		mav.setViewName("demo");
-		return mav;
-	}
+    @Autowired
+    DemoService service;
+
+    @RequestMapping("/demo.do")
+    public ModelAndView demo(HttpServletRequest req, HttpServletResponse resp) {
+        ModelAndView mav = new ModelAndView();
+
+        List<String> idList = service.selectAllId();
+        mav.addObject("idList", idList);
+        mav.setViewName("demo");
+        return mav;
+    }
+
+    @RequestMapping("/rep.do")
+    public String repTest(Model model) {
+        List<HashMap<String, Integer>> repList = service.selectAllRep();
+        System.out.println(repList);
+        model.addAttribute("repList", repList);
+        return "repTest";
+    }
 }
--- src/main/java/com/example/demo/model/DemoMapper.java
+++ src/main/java/com/example/demo/model/DemoMapper.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @Mapper
 public interface DemoMapper {
 	List<String> selectAllId();
+	List<HashMap<String, Integer>> selectAllRep();
 }
