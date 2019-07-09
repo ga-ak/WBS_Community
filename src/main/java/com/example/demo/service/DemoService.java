6d83f076ac15b97a5edc02b6df31a5c13a7238fa
Cheolho kim <gghg0000@gmail.com>
2019-07-09 18:02:50 +0900

    19.07.09 kch_mac
    대댓글 완성(들여쓰기처럼 표현 미작성)

--- src/main/java/com/example/demo/service/DemoService.java
+++ src/main/java/com/example/demo/service/DemoService.java
@@ -19,49 +19,6 @@ public class DemoService {
   @Autowired
   DemoMapper mapper;
 
-  public List<String> selectAllId() {
-    return mapper.selectAllId();
-  }
-
-  public List<HashMap<String, Integer>> selectAllRep() {
-
-    List<HashMap<String, Integer>> result = new ArrayList<>();
-    List<HashMap<String, Integer>> oriArray = mapper.selectAllRep();
-    Stack<HashMap<String, Integer>> stack = new Stack<>();
-
-    System.out.println(oriArray);
-
-
-    // 부모 댓글만 모두 stack 에 담아준다
-    for (int i = oriArray.size() - 1; i >= 0; i--) {
-      HashMap thisRep = oriArray.get(i);
-      if (thisRep.get("rep_parent") == null) {
-        stack.push(thisRep);
-        oriArray.remove(i);
-      }
-    }
-
-    System.out.println(stack);
-
-    while (stack.size() > 0) {
-
-      HashMap<String, Integer> parentRep = stack.pop();
-      result.add(parentRep);
-
-      for (int i = oriArray.size() - 1; i >= 0; i--) {
-        HashMap thisRep = oriArray.get(i);
-        if (parentRep.get("rep_id") == thisRep.get("rep_parent")) {
-          stack.push(thisRep);
-          oriArray.remove(i);
-        }
-      }
-      System.out.println(stack);
-    }
-
-
-    return result;
-  }
-
   public List<MemberVO> selectAllMember() {
     return mapper.selectAllMember();
   }
